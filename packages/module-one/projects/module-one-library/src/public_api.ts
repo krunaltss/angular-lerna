61ff690de4fd7b75c4a1831fc7064e614be9b255
Krunal <krunal.s@thresholdsoft.com>
2018-06-12 10:17:16 +0530

    clean repo

--- packages/module-one/projects/module-one-library/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of module-one-library
- */
-
-export * from './lib/module-one-library.service';
-export * from './lib/module-one-library.component';
-export * from './lib/module-one-library.module';
